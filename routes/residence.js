9159ee0ee95ad3a3614dbd55fe7f6c5b1e8c43b5
RuiyuFang <ryfang1998@163.com>
2018-01-25 17:00:48 +0800

    修改了residence.js，使其可以对应新版小程序发送的数据

--- routes/residence.js
+++ routes/residence.js
@@ -8,7 +8,7 @@ var MongoCLient = require('mongodb').MongoClient, assert = require('assert');
 
 
 var imageBasicLocation = "./public/rawdata/residence/images/"  //所有图片都存放在这个目录下
-var url = 'mongodb://localhost:27017/dashilan'
+var MongodbMongodbUrl = 'mongodb://localhost:27017/dashilan'
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -16,146 +16,111 @@ router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
 
+/* 居民上传表单信息记录
+ *
+ * 1.具体工作：
+ *      直接在mongoDB，dashilan表中插入一条新记录
+ *
+ * 2.log信息：
+ *    例：[REQUEST] 张三 居民提交表单记录
+ *        [REQUEST] 'request的具体信息'
+ *        [REQUEST ERROR] 'error的具体信息'
+ *        [REQUEST] 某个组织 的 张三 居民提交表单记录成功
+ *        
+ */
+router.post('/words', function (req, res, next) {
+    var words = req.body;
+    var name = req.body.name;
+    var organization = req.body.organization;
 
+    console.log('[REQUEST] '+name+' 居民提交表单记录');
+    console.log("[REQUEST] "+req);
+
+    //在mongodb中插入一条新记录
+    MongoCLient.connect(MongodbUrl, function (err, db) {
+        assert.equal(null, err);
+        db.collection("residence").insertOne(words, function (err, res) {
+            console.log("[REQUEST ERROR]"+err);
+            assert.equal(null, err);
+            console.log("[REQUEST] "+organization+" 的 "+name+" 居民提交表单记录成功\n\n");
+            db.close();
+        });
+    });
+
+
+    res.send({ret_code: '0'});
+});
+
+
+/* 居民上传图片
+ *
+ * 1.具体工作：
+ *      ~ 创建图片存储位置的文件夹, 格式为 2018-01-25/组织名称/
+ *      ~ 确定图片的存储名称， 格式为：2018-01-25_组织名称_活动图片_张三_随机数.jpg
+ *      ~ 将确定的文件路径存入mongodb的数据库中对应的那条记录里
+ *
+ *
+ * 2.log信息：
+ *    例：[REQUEST PIC] 居民提交照片
+ *        [REQUEST PIC] 图片存储位置：位置信息
+ *        [REQUEST PIC] 图片存储位置路径创建完成
+ *        [REQUEST PIC] 将新图片位置插入对应的数据库信息中
+ *        [REQUEST PIC] 新图片名称 插入数据库成功
+ *
+ *
+ */
 var storage = multer.diskStorage({
-    //存储文件的位置
+
+    /* 创建存储文件的位置的文件夹 */
     destination: function (req, file, cb) {
         var date = req.body.date;
-        var time = req.body.time;
         var organization = req.body.organization;
-        var type = req.body.type;
-        var location = imageBasicLocation + path.join(date, time, organization, type);
-        console.log(location);
+        var location = imageBasicLocation + path.join(date, organization);
+        console.log("[REQUEST PIC] 图片存储位置："+location);
         fx.mkdir(location, function (err) {
+            // 根据我的猜测，这个cb应该是回调这个标准接口的，把location的信息传给它
             cb(null, location);
-            console.log('make directory done');
+            console.log('[REQUEST PIC] 图片存储位置路径创建完成\n');
         });
     },
 
+    /* 创建存储这个文件的名字 */
     filename: function (req, file, cb) {
         var date = req.body.date;
-        var time = req.body.time;
         var organization = req.body.organization;
+        var name = req.type.name;
         var type = req.body.type;
-        var pic_name = date + '_' + time + '_' + organization + "_" + type + "_" + Math.floor((Math.random() * (10000 - 0 + 1)) + 0).toString() + '.jpg';//存储的文件名
-        var pic_position = path.join(date, time, organization, type, pic_name)
+        var type_str = (type==="activity")?"活动图片":"微创投记录";
+        //存储的文件名
+        var pic_name = date + '_' + organization + "_" + type_str + "_"+ name +"_"+ Math.floor((Math.random() * (10000 - 0 + 1)) + 0).toString() + '.jpg';
+        var pic_position = path.join(date, organization, pic_name)
 
-        MongoCLient.connect(url, function (err, db) {
+        //在mongodb对应的信息里插入图片的位置信息
+        MongoCLient.connect(MongodbUrl, function (err, db) {
             assert.equal(null, err);
-            console.log("begin to insert pic");
+            console.log("[REQUEST PIC] 将新图片位置插入对应的数据库信息中");
             db.collection('residence').update(
-                {$and: [{date: date}, {time: time}, {organization: organization}]},
+                {$and: [{date: date}, {name: name}, {organization: organization}]},
                 {$addToSet: {[type]: pic_position}},
                 function (err, result) {
                     assert.equal(err, null);
-                    console.log("Insert an item into the " + type + " array");
+                    console.log("[REQUEST PIC] "+pic_name+" 插入数据库成功");
 
                 }
             );
         });
 
+        // 我的猜测应该没错，这里是回调上层的接口，把图片的名字传给它
         cb(null, pic_name);
     }
 });
 
-var upload = multer({storage: storage})
+var upload = multer({storage: storage});
 
 
-//文字上传
-router.post('/words', function (req, res, next) {
-    console.log('\n\n');
-    console.log(req.body);
-    var words = req.body;
-    var date = req.body.date;
-    var time = req.body.time;
-    var organization = req.body.organization;
-    var location = req.body.location;
-    var joinNumber = req.body.joinNumber;
-    var theme = req.body.theme;
-    var content = req.body.content;
-    var other = req.body.other;
-    var comment = req.body.comment;
-
-
-    MongoCLient.connect(url, function (err, db) {
-        assert.equal(null, err);
-        var queryCondition = [{date: date}, {organization: organization}];
-        db.collection("residence").find({$and: queryCondition}).toArray(function (err, result) {
-
-            if (err) throw err;
-            //如果已经存在该记录，则用新的值覆盖记录
-            console.log(result);
-            if (result.length != 0) {
-                db.collection("residence").update({$and: queryCondition},
-                    {$addToSet: {comment: comment[0]}},
-                    function (err, result) {
-                        assert.equal(err, null);
-                        console.log("在comment列中增加新的阿姨的感想");
-
-                        //如果还有非空的其他内容，则用这些内容替换掉原来的部分
-
-                        if (location != "") {
-                            db.collection("residence").update({$and: queryCondition},
-                                { $set: {location: location} }, function (err, result) {
-                                    assert.equal(err, null);
-                                    console.log("补充location的值");
-                                });
-                        }
-
-                        if (joinNumber != "") {
-                            db.collection("residence").update({$and: queryCondition},
-                                { $set: {joinNumber: joinNumber} }, function (err, result) {
-                                    assert.equal(err, null);
-                                    console.log("补充joinNumber的值");
-                                });
-                        }
-
-                        if (theme != "") {
-                            db.collection("residence").update({$and: queryCondition},
-                                { $set: {theme: theme} }, function (err, result) {
-                                    assert.equal(err, null);
-                                    console.log("补充主题的的值");
-                                });
-                        }
-
-                        if (content != "") {
-                            db.collection("residence").update({$and: queryCondition},
-                                { $set: {content: content}}, function (err, result) {
-                                    assert.equal(err, null);
-                                    console.log("补充内容的值");
-                                });
-                        }
-
-                        if (other != "") {
-                            db.collection("residence").update({$and: queryCondition},
-                                { $set: {other: other} }, function (err, result) {
-                                    assert.equal(err, null);
-                                    console.log("补充其他的值");
-                                });
-                        }
-
-
-                    });
-            } else {
-
-                db.collection("residence").insertOne(words, function (err, res) {
-                    console.log(err);
-                    assert.equal(null, err);
-                    console.log("插入一条全新的活动记录");
-                    db.close();
-                });
-
-            }
-        });
-    });
-    res.send({ret_code: '0'});
-});
-
-
-// 图片上传
+// 居民图片上传
 router.all('/pic', upload.single('file'), function (req, res, next) {
-    console.log("in main pic");
-    console.log(req.file);
+    console.log("[REQUEST PIC] 居民提交照片\n\n");
     res.send({ret_code: '0'});
 
 });
